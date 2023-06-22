939838abab4775aacfb346742d4161d1df340870
Tomer <tomerrim@gmail.com>
2023-06-22 22:03:13 +0300

    added scoreboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(l_paddle.go_up, "w")
@@ -22,7 +24,7 @@ screen.onkey(r_paddle.go_down, "Down")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     ball.move()
 
     # detect collision with wall
@@ -36,9 +38,15 @@ while game_is_on:
     # detect r_paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # detect l_paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
+
+    if scoreboard.r_score == 5 or scoreboard.l_score == 5:
+        scoreboard.game_over()
+        game_is_on = False
 
 screen.exitonclick()
