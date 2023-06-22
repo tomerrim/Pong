923b05fa10454335e8edcea66e75071c43614ca4
Tomer <tomerrim@gmail.com>
2023-06-22 20:55:44 +0300

    set up the screen, created left and right paddles the can move up and down

---
+++ paddle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, coordinates):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(coordinates)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(x=self.xcor(), y=new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(x=self.xcor(), y=new_y)
