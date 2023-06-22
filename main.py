97e0f1dbfeefe52e51d137800032f986d1eb1700
Tomer <tomerrim@gmail.com>
2023-06-22 21:38:18 +0300

    reset position of ball if it touches left or right side of the screen

--- main.py
+++ main.py
@@ -33,4 +33,12 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    # detect r_paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # detect l_paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 screen.exitonclick()
